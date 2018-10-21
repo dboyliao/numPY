2a54aeff96f6a37dd6d4c82d3c7736bfc2d0a9de
dboyliao <qmalliao@gmail.com>
2018-10-22 02:49:27 +0800

    daily commit

--- numPY/core.py
+++ numPY/core.py
@@ -35,3 +35,36 @@ class NumPyArray:
     @property
     def strides(self):
         return self._strides
+
+    def __getitem__(self, slices):
+        slices = self._validate_slices(slices)
+        viewable = all([isinstance(sl, slice) for sl in slices])
+        if not viewable:
+            return self._make_copy(slices)
+        return self._make_view(slices)
+
+    def __setitem__(self, slices, value):
+        slices = self._validate_slices(slices)
+        print(slices)
+        print(value)
+
+    def __len__(self):
+        return len(self._data)
+
+    def _validate_slices(self, slices):
+        if not isinstance(slices, tuple):
+            slices = (slices,)
+        for _ in range(self._ndim - len(slices)):
+            slices += (slice(None),)
+        if not len(slices) == self._ndim:
+            raise IndexError('too many indices for array')
+        return slices
+
+    def _make_copy(self, indices):
+        pass
+
+    def _make_view(self, indices):
+        pass
+
+    def _iter_slices(self, slices):
+        pass
